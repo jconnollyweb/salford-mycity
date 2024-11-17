c84c99fc78fdfe2af21eec2d19be351c7517e899
jconnollyweb <johnc1983@hotmail.co.uk>
2024-11-17 22:23:48 +0000

    added media query

--- src/components/adultCare/AdultCare.js
+++ src/components/adultCare/AdultCare.js
@@ -1,8 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./AdultCare.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBullhorn,
+  faSchool,
+  faHeartbeat,
+  faWheelchair,
+  faMapMarkerAlt,
+  faHandsHelping,
+} from "@fortawesome/free-solid-svg-icons";
 
 const AdultCareSupport = () => {
   const [posts, setPosts] = useState([]);
+
+  // Map ACF fields to their corresponding icons
+  const iconMap = {
+    advice_and_advocacy: faBullhorn,
+    care_and_support_at_home: faHandsHelping,
+    support_for_carers: faHeartbeat,
+    disability: faWheelchair,
+    education: faSchool,
+    health_services: faHeartbeat,
+    housing_and_accommodation_options: faMapMarkerAlt,
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,23 +41,32 @@ const AdultCareSupport = () => {
   }, []);
 
   return (
-    <div>
+    <div className="adult-care-container">
       <h1>Adult Care and Support</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title.rendered}</h2>
-            {post.acf &&
-              Object.entries(post.acf).map(([field, link]) => (
-                <p key={field}>
-                  <a href={link.url} target={link.target || "_self"}>
+      <div className="card-container">
+        {posts.map((post) =>
+          post.acf
+            ? Object.entries(post.acf).map(([field, link]) => (
+                <div key={field} className="adult-card">
+                  <FontAwesomeIcon
+                    icon={iconMap[field] || faBullhorn} // Default icon if none found
+                    className="card-icon"
+                  />
+                  <h3 className="card-title">
                     {link.title || field.replace(/_/g, " ")}
+                  </h3>
+                  <a
+                    href={link.url}
+                    target={link.target || "_self"}
+                    className="card-link"
+                  >
+                    Learn More
                   </a>
-                </p>
-              ))}
-          </li>
-        ))}
-      </ul>
+                </div>
+              ))
+            : null
+        )}
+      </div>
     </div>
   );
 };
